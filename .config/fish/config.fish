82c7c4453f2ae1a7735cb8a374583df884eb137b
helixoid <ayushrajmth91@gmail.com>
2023-07-24 19:39:40 +0530

    Config Cleanup & MPV Improvements

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 neofetch
 starship init fish | source
+alias prop="hyprprop | grep -i 'class\|title\|xwayland'"
+export "MICRO_TRUECOLOR=1"
 end
