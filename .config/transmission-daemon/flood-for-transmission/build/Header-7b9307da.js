a1d00937ef6f3149aeeda31716262101cd7bd069
helixoid <ayushrajmth91@gmail.com>
2023-08-06 13:48:42 +0530

    Transmission Daemon & WebUI Implementation

---
+++ .config/transmission-daemon/flood-for-transmission/build/Header-7b9307da.js
@@ -0,0 +1,2 @@
+import{S as s,i as t,s as a,e,U as n,h as r,j as c,l as i,aA as o,at as u,u as l}from"./main.js";function x(s){let t,a;return{c(){t=e("h2"),a=n(s[0]),r(t,"class","svelte-1kue5c1")},m(s,e){c(s,t,e),i(t,a)},p(s,[t]){1&t&&o(a,s[0])},i:u,o:u,d(s){s&&l(t)}}}function m(s,t,a){let{text:e}=t;return s.$$set=s=>{"text"in s&&a(0,e=s.text)},[e]}class p extends s{constructor(s){super(),t(this,s,m,x,a,{text:0})}}export{p as H};
+//# sourceMappingURL=Header-7b9307da.js.map
