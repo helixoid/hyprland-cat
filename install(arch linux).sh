4a17e479d7f0e85ebf45ca44e9c87873ac4dfbac
helixoid <ayushrajmth91@gmail.com>
2023-08-14 20:56:46 +0530

    Cantata Included

--- install(arch linux).sh
+++ install(arch linux).sh
@@ -17,7 +17,7 @@ makepkg -si
 # Installing Necessary Packages
 paru -S --needed --noconfirm hyprland
 
-paru -S --needed --noconfirm waybar wofi mako dolphin sddm nwg-look-bin mpv yt-dlp kitty wlsunset wlogout swaylock-effects kvantum qt5ct qt6ct qt5-wayland qt6-wayland transmission-cli telegram-desktop sublime-text-4 swayidle swaybg mpd ncmpcpp ark okular neofetch network-manager-applet bottom pavucontrol vnstat wl-clipboard cliphist gnome-disk-utility fish starship firewalld brightnessctl imv noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-jetbrains-mono-nerd intel-gpu-tools polkit-kde-agent xdg-desktop-portal-hyprland tremc-git grimblast newsboat speech-dispatcher bat plymouth
+paru -S --needed --noconfirm waybar wofi mako cantata dolphin sddm nwg-look-bin mpv yt-dlp kitty wlsunset wlogout swaylock-effects kvantum qt5ct qt6ct qt5-wayland qt6-wayland transmission-cli telegram-desktop sublime-text-4 swayidle swaybg mpd ncmpcpp ark okular neofetch network-manager-applet bottom pavucontrol vnstat wl-clipboard cliphist gnome-disk-utility fish starship firewalld brightnessctl imv noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-jetbrains-mono-nerd intel-gpu-tools polkit-kde-agent xdg-desktop-portal-hyprland tremc-git grimblast newsboat speech-dispatcher bat plymouth
 
 # Installing Intel Hardware Decoding Driver
 paru -S --needed --noconfirm intel-media-driver
