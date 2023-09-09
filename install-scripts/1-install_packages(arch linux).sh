664f55fa2ea6e9b157e064f8c2735f26f49170b5
helixoid <ayushrajmth91@gmail.com>
2023-09-09 22:18:30 +0530

    Install Scripts Improvements

--- install-scripts/1-install_packages(arch linux).sh
+++ install-scripts/1-install_packages(arch linux).sh
@@ -3,13 +3,13 @@
 # Installing Necessary Packages
 paru -S --needed --noconfirm hyprland waybar rofi-lbonn-wayland-git xdg-desktop-portal-hyprland dunst
 
-paru -S --needed --noconfirm nwg-look-bin mpv yt-dlp
+paru -S --needed --noconfirm nwg-look-bin mpv yt-dlp mpd ncmpcpp
 
 paru -S --needed --noconfirm kitty swaylock-effects kvantum qt5ct qt6ct 
 
 paru -S --needed --noconfirm wlsunset qt5-wayland qt6-wayland transmission-cli telegram-desktop
 
-paru -S --needed --noconfirm swayidle swaybg mpd mpc ncmpcpp
+paru -S --needed --noconfirm swayidle swaybg python-geoip
 
 paru -S --needed --noconfirm okular neofetch network-manager-applet bottom
 
@@ -23,7 +23,7 @@ paru -S --needed --noconfirm  polkit-kde-agent tremc-git grim bemoji
 
 paru -S --needed --noconfirm slurp swappy pamixer newsboat bat
 
-paru -S --needed --noconfirm papirus-icon-theme ttf-jetbrains-mono ttf-nerd-fonts-symbols-mono
+paru -S --needed --noconfirm papirus-icon-theme otf-font-awesome
 
 #paru -S --needed --noconfirm sddm plymouth bibata-cursor-theme-bin sddm-theme-sugar-candy-git sublime-text-4 ark
 
--- install-scripts/2-install_dotfiles.sh
+++ install-scripts/2-install_dotfiles.sh
@@ -3,9 +3,7 @@
 # Installing Dotfiles
 cd $HOME/hyprland-cat
 cp -R .config ~/
-cp -R .icons ~/
 cp -R .themes ~/
-cp -R fonts ~/.local/share/
 cp -R .newsboat ~/
 cp -R Wallpapers ~/
 cd
