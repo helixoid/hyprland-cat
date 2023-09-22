d2a3440291839c34307816f67ebae7ef4305714a
helixoid <ayushrajmth91@gmail.com>
2023-09-22 22:17:14 +0530

    Script Improvements

--- install-scripts/0-paru_install.sh
+++ install-scripts/0-paru_install.sh
@@ -8,6 +8,7 @@ sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin
 makepkg -si 
+cd
 
 # Endeavour OS
 #yay -S paru-bin
--- install-scripts/1-install_packages(arch linux).sh
+++ install-scripts/1-install_packages(arch linux).sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Installing Necessary Packages
-paru -S --needed --noconfirm hyprland waybar rofi-lbonn-wayland-git xdg-desktop-portal-hyprland dunst
+paru -S --needed --noconfirm hyprland waybar rofi-lbonn-wayland-git xdg-desktop-portal-hyprland
 
 paru -S --needed --noconfirm nwg-look-bin mpv yt-dlp mpd ncmpcpp
 
@@ -9,7 +9,7 @@ paru -S --needed --noconfirm kitty swaylock-effects kvantum qt5ct qt6ct
 
 paru -S --needed --noconfirm wlsunset qt5-wayland qt6-wayland transmission-cli telegram-desktop
 
-paru -S --needed --noconfirm swayidle swaybg python-geoip
+paru -S --needed --noconfirm swayidle swaybg python-geoip swaync
 
 paru -S --needed --noconfirm okular neofetch network-manager-applet bottom
 
@@ -21,11 +21,11 @@ paru -S --needed --noconfirm noto-fonts noto-fonts-cjk noto-fonts-extra noto-fon
 
 paru -S --needed --noconfirm  polkit-kde-agent tremc-git grim bemoji
 
-paru -S --needed --noconfirm slurp swappy pamixer newsboat bat
+paru -S --needed --noconfirm slurp swappy newsboat bat
 
 paru -S --needed --noconfirm papirus-icon-theme otf-font-awesome
 
-#paru -S --needed --noconfirm sddm plymouth bibata-cursor-theme-bin sddm-theme-sugar-candy-git sublime-text-4 ark
+#paru -S --needed --noconfirm sddm plymouth sddm-theme-sugar-candy-git
 
 # Installing Intel Hardware Decoding Driver
 paru -S --needed --noconfirm intel-media-driver
@@ -34,5 +34,6 @@ paru -S --needed --noconfirm intel-media-driver
 # Removing Extra Packages
 paru -Rns vim
 paru -Rns xdg-desktop-portal-gnome
+paru -Rns xdg-desktop-portal-kde
 paru -Rns xdg-desktop-portal-gtk
 paru -c
