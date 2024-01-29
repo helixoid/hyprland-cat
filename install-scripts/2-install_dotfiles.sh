0a2b94d9bd2da85b65d99b443da74fcddc2a2691
helixoid <ayushrajmth91@gmail.com>
2024-01-29 16:12:16 +0530

    Wallpapers & Install Script Improvements

--- install-scripts/2-install_dotfiles.sh
+++ install-scripts/2-install_dotfiles.sh
@@ -5,7 +5,8 @@ xdg-user-dirs-update
 # Installing Dotfiles
 cd $HOME/hyprland-cat
 cp -R .config ~/
-cp -R .themes ~/
 cp -R .newsboat ~/
 cp -R Wallpapers ~/
 cd
+
+#wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.local/share/icons" sh
