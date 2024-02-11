7600629604b4c6550d47b73b4ab72ebff2688016
helixoid <ayushrajmth91@gmail.com>
2024-02-11 13:38:23 +0530

    Alacritty Terminal is now default.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 macchina
 starship init fish | source
+alias v="nvim"
 alias hx="helix"
-alias prop="hyprprop | grep -i 'class\|title\|xwayland'"
+alias prop="hyprctl clients | grep -i 'class\|title\|xwayland'"
 alias ls='lsd'
 alias l='ls -l'
 alias la='ls -a'
